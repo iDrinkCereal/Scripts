8033c3637304cebea5ae5b86de67b160f3292139
iDrinkCereal <62197411+iDrinkCereal@users.noreply.github.com>
2020-03-16 16:25:59 +1030

    Very OP Counter Blox GUI
    easy to use and OP

---
+++ Counter Blox GUI.lua
@@ -0,0 +1,37 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/CriShoux/OwlHub/master/OwlHub.txt"))();
+--[[Arsenal
+A bizzare day
+Assassin
+Bad Business
+Bullet Hell
+Big PaintBall!
+Counter Blox
+Esper Online
+Island Royale
+INFINITY RPG 2
+JoJo Blox
+Knife Ability Test | KAT
+Lifting Simulator
+Muscle Legend
+Mad PaintBall 2
+Ninja Legends
+No Scope Sniping
+One Piece Legendary
+One Punch Man
+Operation Scorpion
+One Piece Ultimate
+PowerSimulator
+Prison Life
+Phantom Forces
+Project Lazarus
+Ruddevs Battle Royale
+R2DA
+Redwood Prison
+Stands Online
+Strucid
+Skywars
+Sound Space
+Vehicle Tycoon
+Wild Revolvers
+Zombie Rush
+Zombie Strike--]]
